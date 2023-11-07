0a3b14e6c45fd439d509c42a985659c9b84682fe
Robert <robert.begier@gmail.com>
2023-11-07 19:58:35 +0100

    next devs

--- app/helpers/scriptures_helper.rb
+++ app/helpers/scriptures_helper.rb
@@ -1,2 +1,5 @@
 module ScripturesHelper
+  def first_scripture_in_chapther(b,c)
+    Scripture.where(book: b, chapter: c).minimum(:id)
+  end
 end
--- app/models/scripture.rb
+++ app/models/scripture.rb
@@ -5,6 +5,10 @@ class Scripture < ApplicationRecord
     "#{book} #{chapter}:#{verse}"
   end
 
+  def chapters_in_book_of_scripture
+    Scripture.where(book: book).pluck(:chapter).uniq
+  end
+
   def previous_chapters_end
     f = Scripture.where(book: book, chapter: chapter).minimum(:id)
     if f <= 1
