5ed17a12198b188cd759724e91d0331116f75d5b
Robert <robert.begier@gmail.com>
2023-11-08 16:01:45 +0100

    devs

--- app/models/scripture.rb
+++ app/models/scripture.rb
@@ -14,7 +14,13 @@ class Scripture < ApplicationRecord
     if f <= 1
       false
     else 
-      f-1
+      #last_scripture_in_preceeding_book = Scripture.find(f-1)
+      #[(f-1), "<< #{last_scripture_in_preceeding_book.book}:#{last_scripture_in_preceeding_book.chapter}"]
+      last_scripture_in_previous_book = Scripture.find(f-1)
+      book_of_last_previous_scripture = last_scripture_in_previous_book.book
+      chapter_of_last_previous_scripture = last_scripture_in_previous_book.chapter
+      id_first_scripture_in_previous_chapter = Scripture.where(book: book_of_last_previous_scripture, chapter: chapter_of_last_previous_scripture).minimum(:id)
+      [id_first_scripture_in_previous_chapter, "<< #{book_of_last_previous_scripture}:#{chapter_of_last_previous_scripture}"]
     end
   end
 
@@ -23,7 +29,8 @@ class Scripture < ApplicationRecord
     if f >= 31098
       false
     else 
-      f+1
+      first_scripture_in_next_book = Scripture.find(f+1)
+      [(f+1), "#{first_scripture_in_next_book.book}:#{first_scripture_in_next_book.chapter} >>"]
     end
   end
 end
