a4fef36b587226cade07c20117a077d16a8b858f
Robert <robert.begier@gmail.com>
2023-11-08 20:30:22 +0100

    devs

--- app/helpers/scriptures_helper.rb
+++ app/helpers/scriptures_helper.rb
@@ -2,4 +2,8 @@ module ScripturesHelper
   def first_scripture_in_chapther(b,c)
     Scripture.where(book: b, chapter: c).minimum(:id)
   end
+
+  def first_scripture_in_book(b)
+    Scripture.where(book: b).minimum(:id)
+  end
 end
